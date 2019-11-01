d7fcd470a81b30634572e45fcc0d927b416cd10d
Kim Christensen <ktc@kb.dk>
2019-11-01 13:05:10 +0100

    add some debug info to test script

--- test-index.sh
+++ test-index.sh
@@ -4,6 +4,10 @@ SOLR_HOST=$1
 
 echo "Solr host is to be found on: '$SOLR_HOST'"
 
+echo "curl http://$SOLR_HOST/solr/ds/select?q=*%3A*"
+curl "http://$SOLR_HOST/solr/ds/select?q=*%3A*"
+sleep 10
+
 curl "http://$SOLR_HOST/solr/ds/select?q=*%3A*"
 
 echo "Toes here's to you ;)"
