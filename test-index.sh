cd0fabe880c7549513a48c66ba482bbca94911e7
Kim Christensen <ktc@kb.dk>
2019-11-01 12:57:19 +0100

    Don't use port number when relying on external route

--- test-index.sh
+++ test-index.sh
@@ -4,7 +4,7 @@ SOLR_HOST=$1
 
 echo "Solr host is to be found on: '$SOLR_HOST'"
 
-curl "http://$SOLR_HOST:10007/solr/ds/select?q=*%3A*"
+curl "http://$SOLR_HOST/solr/ds/select?q=*%3A*"
 
 echo "Toes here's to you ;)"
 
