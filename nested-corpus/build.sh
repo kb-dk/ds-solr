0818c5f2786b37feb496e650fd37f8e036e23c1f
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-09-28 11:07:22 +0200

    just examples

---
+++ nested-corpus/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SAXON="java -jar /home/slu/saxon/saxon9he.jar "
+
+mods_files=("albert-einstein.xml"
+	    "hvidovre-teater.xml"
+	    "simonsen-brandes.xml"
+	    "tystrup-soroe.xml")
+
+for file in ${mods_files[@]}; do
+    json=$(echo $file | perl -pe 's/.xml/.json/g')
+    $SAXON -xsl:mods2solr.xsl -s:"$file" | jq > "$json"
+done
