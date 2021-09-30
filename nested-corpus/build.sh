41b34efa9b1cb8351d984fb50b4d135d869e6ae2
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-09-30 13:50:59 +0200

    added ms thing to build.sh

--- nested-corpus/build.sh
+++ nested-corpus/build.sh
@@ -5,7 +5,8 @@ SAXON="java -jar /home/slu/saxon/saxon9he.jar "
 mods_files=("albert-einstein.xml"
 	    "hvidovre-teater.xml"
 	    "simonsen-brandes.xml"
-	    "tystrup-soroe.xml")
+	    "tystrup-soroe.xml"
+	    "homiliae_super_psalmos.xml")
 
 for file in ${mods_files[@]}; do
     json=$(echo $file | perl -pe 's/.xml/.json/g')
