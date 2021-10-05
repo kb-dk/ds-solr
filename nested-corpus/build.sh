d5b4e18741eb9fce243992a3d3ba748896d4e7bc
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-10-05 15:38:48 +0200

    adding these texts to build.sh

--- nested-corpus/build.sh
+++ nested-corpus/build.sh
@@ -6,7 +6,9 @@ mods_files=("albert-einstein.xml"
 	    "hvidovre-teater.xml"
 	    "simonsen-brandes.xml"
 	    "tystrup-soroe.xml"
-	    "homiliae-super-psalmos.xml")
+	    "homiliae-super-psalmos.xml"
+	    "work_on_logic.xml"
+	    "responsa.xml")
 
 for file in ${mods_files[@]}; do
     json=$(echo $file | perl -pe 's/.xml/.json/g')
