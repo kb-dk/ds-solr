12bcccaf3a171828ed0a5edc4b719a1428235c78
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-10-21 09:59:17 +0200

    using the default solrconfig.xml

---
+++ nested-corpus/load_another.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/albert-einstein.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/simonsen-brandes.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/hvidovre-teater.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/tystrup-soroe.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/responsa.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/work_on_logic.json
+curl "http://localhost:10007/solr/another_ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/homiliae-super-psalmos.json
