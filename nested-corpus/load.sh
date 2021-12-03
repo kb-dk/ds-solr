f2066dea720c22e1a9da6cf8795c2dfcd3ebdac2
Toke Eskildsen <toes@kb.dk>
2021-12-03 09:19:27 +0100

    Ensure scripts can be called from anywhere

--- nested-corpus/load.sh
+++ nested-corpus/load.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pushd ${BASH_SOURCE%/*} > /dev/null
+cd ..
+
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/albert-einstein.json
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/simonsen-brandes.json
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/hvidovre-teater.json
@@ -7,3 +10,5 @@ curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: appli
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/responsa.json
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/work_on_logic.json
 curl "http://localhost:10007/solr/ds/update?commit=true" -H "Content-Type: application/json" --data-binary @nested-corpus/homiliae-super-psalmos.json
+
+popd > /dev/null
